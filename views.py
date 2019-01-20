daf998599f1f5be8d65ec5ce0fb2a0df19c4a0b4
RichardHarleyson <richard.harleyson@gmail.com>
2019-01-21 00:00:40 +0200

    Setted base page tempalte, main page and its indexing

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse('Hello there')
+	# return HttpResponse('Hello there')
+	data = {'data': 'Sir Annoy-0'}
+	return render(request, 'index.html', context=data)
