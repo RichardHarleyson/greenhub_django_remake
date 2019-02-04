6dbb49422835aed64e558966030bc28a18d00197
RichardHarleyson <richard.harleyson@gmail.com>
2019-02-04 22:01:56 +0200

    Deleted Contacts, Charger_map and Core app folders

--- apps/charger_map/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChargerMapConfig(AppConfig):
-    name = 'charger_map'
--- apps/charger_map/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-	return render(request, 'charger_map/base_charger_map.html')
