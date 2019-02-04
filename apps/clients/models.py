f7f8c91e32d14b792090ba3ff8843781ad3d8e1d
RichardHarleyson <richard.harleyson@gmail.com>
2019-02-04 18:13:15 +0200

    Added core app

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Clients_main(models.Model):
-	client_name = models.charfield(maxlength=50, null=True, blank=True)
-	client_phone = models.charfield(maxlenght=20)
-	client_stage = models.charfield(maxlength=20, null=True, blank=True)
-	client_status = models.booleanfield()
-	client_join_date = models.DateTimeField()
+	client_name = models.CharField(max_length=50, null=True, blank=True)
+	client_phone = models.CharField(max_length=20)
+	client_stage = models.CharField(max_length=20, null=True, blank=True)
+	client_status = models.BooleanField()
+	client_join_date = models.DateTimeField(auto_now_add=True)
